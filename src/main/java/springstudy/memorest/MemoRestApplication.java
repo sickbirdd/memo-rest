98228205925021430b3146c5eaf51addfada024b
ByeongIk Choi <bichoi0715@gmail.com>
2024-04-04 22:09:12 +0900

    init: 스프링 프로젝트 설정

---
+++ src/main/java/springstudy/memorest/MemoRestApplication.java
@@ -0,0 +1,13 @@
+package springstudy.memorest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springstudy/memorest/MemoRestApplicationTests.java
@@ -0,0 +1,13 @@
+package springstudy.memorest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
